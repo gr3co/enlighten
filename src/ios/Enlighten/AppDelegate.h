9a84ecee03d85f9bcb96804639d8ae83c94a2cf3
Stephen Greco <sgreco94@gmail.com>
2015-02-14 13:17:24 -0500

    Created new Xcode project for iOS app

---
+++ src/ios/Enlighten/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Enlighten
+//
+//  Created by Stephen Greco on 2/12/15.
+//  Copyright (c) 2015 18-549 Team10. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ src/ios/Enlighten/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Enlighten
+//
+//  Created by Stephen Greco on 2/12/15.
+//  Copyright (c) 2015 18-549 Team10. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
